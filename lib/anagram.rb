9f7ca09e2caf65b9d7b65476524f321a3dba36a8
Luke Chen <luke.chen.2010@gmail.com>
2020-05-07 19:17:05 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+
+class Anagram
+
+  def initialize(new_word)
+    @word = new_word
+  end
+  
+  def match(word_list)
+    match_list = []
+    
+    word_list.each do |x|
+        match_list << x  if @word.split("").sort == x.split("").sort
+    end
+    
+    return match_list
+  end
+    
+end
