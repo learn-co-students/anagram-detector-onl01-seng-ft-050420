da7432e6f0f65c71f9e12db241ad70d415f05373
David Hutchison <dhutchis68516@gmail.com>
2020-05-15 16:25:23 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :words
+
+  def initialize(words)
+    @words = words
+  end
+
+  def match(mixed_word)
+    mixed_word.select {|mixed_word| @words.split("").sort == mixed_word.split("").sort}
+  end
+end
