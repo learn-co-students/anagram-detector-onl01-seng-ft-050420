1ee24b946b99396491b9889c5dd0ae5264645a36
Raveen K Sharma <raveen08@gmail.com>
2020-05-17 06:24:08 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
+
 # Your code goes here!
+
+class Anagram
+  attr_reader :grams
+
+  def initialize(grams)
+   @grams = grams
+
+  end 
+
+  def match(array)
+    
+    array.select do |element|
+      (@grams.split("").sort) == (element.split("").sort)
+    end
+  end
+end
